70669314c4ab4741f25534d68394ee806c6378e4
GuilermoT <72135202+GuilermoT@users.noreply.github.com>
2022-03-03 09:55:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,47 @@
-<table align = "center" border = '1' width = "100%">
-
 <?php
-    function printMultiplicationTable($number){
-        for($i = 0; $i <= 10; $i++){
-            echo "<tr>";
-            $multiplicationTable = ($i * $number);
-            echo "<td>$number  x $i =  $multiplicationTable </td>";
-            echo "<tr/>";
+    function esPrimo($n){
+        $primo = true;
+        for($i = 2; $i < $n; $i++){
+            if($n % $i == 0){
+                $primo = false;
+                break;
+            }
+        }
+        return $primo;
+    }
+
+    function listaPrimos($n1, $n2){
+        $absN1 = abs($n1);
+        $absN2 = abs($n2);
+        $j = 0;
+        $listaPrimos[] = array();
+
+        if($absN1 >= $absN2){
+            for($i = $absN2; $i <= $absN1; $i++){
+                if(esPrimo($i)){
+                    $listaPrimos[$j] = $i;
+                    $j++;
+                }
+            }
+        } else{
+            for($i = $absN1; $i <= $absN2; $i++){
+                if(esPrimo($i)){
+                    $listaPrimos[$j] = $i;
+                    $j++;
+                }
+            }
+        }
+        return $listaPrimos;
+    }
+
+    function imprimirArray($arr){
+        foreach ($arr as $value) {
+            echo "<li>$value</li>";
         }
     }
 
-    $number = $_POST['number'];
-    printMultiplicationTable($number);
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    print("Lista de números primos comprendidos entre " . abs($n1) . " y " . abs($n2) . ":\n");
+    imprimirArray(listaPrimos($n1, $n2));
 ?>
